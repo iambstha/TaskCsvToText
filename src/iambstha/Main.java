b5d40af7cbfa71f1bee555bd62013f04f61c50a5
iambstha <iambstha@gmail.com>
2024-02-20 17:40:23 +0545

    today

--- src/iambstha/Main.java
+++ src/iambstha/Main.java
@@ -3,8 +3,6 @@ package iambstha;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.security.Timestamp;
-import java.time.Instant;
 import java.util.Date;
 
 import iambstha.utils.TaskExporter;
@@ -24,13 +22,13 @@ public class Main implements TaskHandler {
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Date ts = new Date();
 		File file = new File("src/task.csv");
 		FileReader fileReader = new FileReader(file);
 		String outputFilePath = "src/task-records/";
 		ts.toInstant();
-		String outputFileName = ts.toInstant().toString().substring(0,10) + "_task.txt";
+		String outputFileName = ts.toInstant().toString().substring(0, 10) + "_task.txt";
 
 		Main main = new Main();
 		main.convert(fileReader, outputFilePath, outputFileName);
